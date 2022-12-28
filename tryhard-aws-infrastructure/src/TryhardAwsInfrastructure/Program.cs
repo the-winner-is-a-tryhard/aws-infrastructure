6adfa1d6b962f2e6897a3724ecae7d79e3a12413
Scottie Enriquez <scottie@enriquez.xxx>
2022-12-28 17:02:58 -0600

    Adding CDK skeleton

---
+++ tryhard-aws-infrastructure/src/TryhardAwsInfrastructure/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.CDK;
+
+namespace TryhardAwsInfrastructure
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new AvatarCdkStack(app, "AvatarCdkStack");
+            app.Synth();
+        }
+    }
+}
